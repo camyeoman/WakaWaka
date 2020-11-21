7233b26e065779a5aee77902e866092af1ee3c9b
Campbell Yeoman <cam@yeoman.com.au>
2020-11-21 15:02:34 +1100

    stable, but need to clean a little

--- src/test/java/ghost/AgentTest.java
+++ src/test/java/ghost/AgentTest.java
@@ -9,53 +9,78 @@ interface lambdaTest<T, U, V> {
 	public void with(T a, U b, V c);
 }
 
-public class AgentTest {
+public class AgentTest extends TestTools {
+	static {
+		Configuration config = new Configuration();
+		config.spriteMap = testMap;
+		config.speed = 1;
+
+		Agent.SETUP(config);
+	}
 
 	@Test
 	public void constructor()
 	{
 		Agent agent = new Agent(1, 1);
 		assertNotNull(agent);
+		assertNull(agent.direction());
+		assertEquals(agent.intialPoint, new Point(1, 1));
+		assertEquals(agent.point(), new Point(1, 1));
 	}
 
 	@Test
-	public void translate()
-	{
-		Agent.setup(testMap, 1);
+	public void translate() {
+		Point[] points = new Point[]{
+			new Point(16,  16), new Point(32,  16), new Point(48,  16),
+			new Point(64,  16), new Point(80,  16), new Point(96,  16)
+		};
 
-		lambdaTest<Integer, Integer, Point> pointCheck
-			= (a, b, point) -> assertTrue(point.x == a && point.y == b);
+		for (Point point : points) {
+			Agent agent = new Agent(point.x, point.y);
 
-		Agent agent = new Agent(16, 16);
+			for (int i=-20; i < 20; i++) {
+				assertEquals(new Point(point.x+i, point.y), agent.translate(Direction.right, i));
+				assertEquals(new Point(point.x-i, point.y), agent.translate(Direction.left, i));
+				assertEquals(new Point(point.x, point.y+i), agent.translate(Direction.down, i));
+				assertEquals(new Point(point.x, point.y-i), agent.translate(Direction.up, i));
 
-		pointCheck.with(16,16, agent.translate(null, 1));
-
-		// test right
-		for (int i=-20; i < 20; i++) {
-			pointCheck.with(16+i, 16, agent.translate(Direction.right, i));
+				// null case
+				assertEquals(new Point(point.x, point.y), agent.translate(null, i));
+			}
 		}
+	}
 
-		// test left
-		for (int i=-20; i < 20; i++) {
-			pointCheck.with(16-i, 16, agent.translate(Direction.left, i));
-		}
+	@Test
+	public void softReset() {
+		Point[] points = new Point[]{
+			new Point(16,  16), new Point(32,  16), new Point(48,  16),
+			new Point(64,  16), new Point(80,  16), new Point(96,  16)
+		};
 
-		// test down
-		for (int i=-20; i < 20; i++) {
-			pointCheck.with(16, 16+i, agent.translate(Direction.down, i));
-		}
+		for (Point point : points) {
+			Agent agent = new Agent(point.x, point.y);
 
-		// test up
-		for (int i=-20; i < 20; i++) {
-			pointCheck.with(16, 16-i, agent.translate(Direction.up, i));
+			for (int i=-20; i < 20; i++) {
+				Point newPoint = agent.translate(Direction.right, i);
+				agent.direction = Direction.values()[Math.abs(i % 4)];
+				agent.x = newPoint.x;
+				agent.y = newPoint.y;
+
+				agent.softReset();
+				assertEquals(agent.intialPoint, agent.point());
+				assertEquals(agent.intialPoint, point);
+			}
 		}
+	}
+
+	public void SETUP() {
+		Configuration config = new Configuration();
+		config.speed = 5;
 	}
 
 	@Test
 	public void validDirection()
 	{
-		Agent.setup(testMap, 1);
-
 		Agent agent;
 
 		// test behavuiour on grid squares, i.e % 16 == 0
@@ -98,8 +123,6 @@ public class AgentTest {
 	@Test
 	public void validDirections()
 	{
-		Agent.setup(testMap, 1);
-
 		Agent agent;
 
 		testValidDirections(new Boolean[]{false, true, false, true}, new Agent(16*8, 16*3));
@@ -117,20 +140,18 @@ public class AgentTest {
 		testValidDirections(new Boolean[]{ true, false, false, true }, agent);
 	}
 
+	/*
 	@Test
 	public void getterAndSetterMethods()
 	{
-		Agent.setup(testMap, 1);
-
-		int[][] tests = new int[][]{
-			{16,  16}, {32,  16}, {48,  16},
-			{64,  16}, {80,  16}, {96,  16}
+		Point[] points = new Point[]{
+			new Point(16,  16), new Point(32,  16), new Point(48,  16),
+			new Point(64,  16), new Point(80,  16), new Point(96,  16)
 		};
 
-		for (int[] coord : tests) {
-			int a = coord[0], b = coord[1];
-			Agent agent = new Agent(a, b);
-			Direction direction = agent.getDirection();
+		for (Point point : points) {
+			Agent agent = new Agent(point.x, point.y);
+			Direction direction = agent.direction();
 
 			assertEquals(agent.displayX(), a - 5); 
 			assertEquals(agent.displayY(), b - 5); 
@@ -143,21 +164,21 @@ public class AgentTest {
 			assertEquals(agent.toString(), String.format("(%s, %s) heading right", a, b));
 		}
 
+	}
+
 		Agent agent = new Agent(16, 16);
 		for (int i=-10; i < 10; i++) {
-			Agent.setup(testMap, i);
 			if (i==2||i==1) {
 				assertTrue(Agent.speed == i);
 			} else {
 				assertTrue(Agent.speed == 1 || Agent.speed == 2);
 			}
 		}
-	}
+	*/
 
 	// Testing functions
 
-	private void testValidDirection(Boolean[] expected, Agent agent)
-	{
+	private void testValidDirection(Boolean[] expected, Agent agent) {
 		for (int i=0; i < 4; i++) {
 			Direction testDirection = Direction.values()[i];
 
@@ -171,8 +192,7 @@ public class AgentTest {
 		}
 	}
 
-	private void testValidDirections(Boolean[] expected, Agent agent)
-	{
+	private void testValidDirections(Boolean[] expected, Agent agent) {
 		for (int i=0; i < 4; i++) {
 			Direction testDirection = Direction.values()[i];
 			boolean contained = agent.validDirections().contains(testDirection);
@@ -185,25 +205,4 @@ public class AgentTest {
 		}
 	}
 
-	static Sprite[][] testMap;
-
-	static // test map
-	{
-		String[][] stringMap = new String[][] {
-			{ "3","1","1","1","1","1","1","1","1","1","1","1","4" },
-			{ "2","p","a","c","i","w","7","7","7","7","7","7","2" },
-			{ "2","1","1","1","1","1","1","1","1","4","7","7","2" },
-			{ "2","2","2","7","7","2","7","7","7","2","7","7","2" },
-			{ "2","7","2","2","7","7","7","2","7","7","7","7","2" },
-			{ "5","1","1","1","1","1","1","1","1","1","1","1","6" }
-		};
-
-		testMap = new Sprite[6][13];
-
-		for (int j=0; j < 6; j++) {
-			for (int i=0; i < 13; i++) {
-				testMap[j][i] = Sprite.getSprite(stringMap[j][i]);
-			}
-		}
-	}
 }
