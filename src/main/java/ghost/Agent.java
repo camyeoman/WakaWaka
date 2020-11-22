2757b7e848f953cf86d65ca6720e92ab4d8a7897
Campbell Yeoman <cam@yeoman.com.au>
2020-11-22 14:07:46 +1100

    Added comments for variables, note check if static need comments

--- src/main/java/ghost/Agent.java
+++ src/main/java/ghost/Agent.java
@@ -3,14 +3,33 @@ package ghost;
 import java.util.List;
 import java.util.ArrayList;
 
+/**
+ * Super class to Player and Ghost, containing most of the basic functionality.
+ */
 public class Agent extends Coordinate {
-	protected static Sprite[][] spriteMap;
+	/**
+	 * A map of sprites representing the game map.
+	 */
+	protected static Sprite[][] SPRITE_MAP;
+
+	/**
+	 * An integer value representing the speed.
+	 */
 	protected static int speed = 0;
 
-	// Corners of Map
+	/**
+	 * Corners of SPRITE_MAP.
+	 */
 	static Point BOT_RIGHT, TOP_RIGHT, BOT_LEFT, TOP_LEFT;
 
+	/**
+	 * Current direction.
+	 */
 	protected Direction direction;
+
+	/**
+	 * Initial position as a point.
+	 */
 	final Point intialPoint;
 
 	/**
@@ -31,12 +50,12 @@ public class Agent extends Coordinate {
 	 * @param config, a configuration object with the needed data
 	 */
 	public static void SETUP(Configuration config) {
-		Agent.spriteMap = config.spriteMap;
+		Agent.SPRITE_MAP = config.spriteMap;
 
 		// Get Corners
-		BOT_RIGHT = new Point(16 * spriteMap[0].length, 16 * spriteMap.length);
-		TOP_RIGHT = new Point(16 * spriteMap[0].length, 0);
-		BOT_LEFT = new Point(0 , 16 * spriteMap.length);
+		BOT_RIGHT = new Point(16 * SPRITE_MAP[0].length, 16 * SPRITE_MAP.length);
+		TOP_RIGHT = new Point(16 * SPRITE_MAP[0].length, 0);
+		BOT_LEFT = new Point(0 , 16 * SPRITE_MAP.length);
 		TOP_LEFT = new Point(0 , 0);
 
 		if (config.speed == 1 || config.speed == 2) {
@@ -47,6 +66,14 @@ public class Agent extends Coordinate {
 	// Getter and Setter methods
 
 	/**
+	 * Returns current direction.
+	 * @return current direction
+	 */
+	public Direction direction() {
+		return direction;
+	}
+
+	/**
 	 * Resets an agent to initial position and resets direction. This is used
 	 * when a player loses a life to a ghost, and this method is partially
 	 * overwritten in the sublcasses Player and ghost.
@@ -55,14 +82,6 @@ public class Agent extends Coordinate {
 		this.x = intialPoint.x;
 		this.y = intialPoint.y;
 		this.direction = null;
-	}
-
-	/**
-	 * Returns current direction.
-	 * @return current direction
-	 */
-	public Direction direction() {
-		return direction;
 	}
 
 	// Position and Direction methods
@@ -105,7 +124,7 @@ public class Agent extends Coordinate {
 			Point point = translate(newDirection, 1).gridSnap(newDirection);
 
 			try {
-				Sprite sprite = spriteMap[point.y/16][point.x/16];
+				Sprite sprite = SPRITE_MAP[point.y/16][point.x/16];
 				return !sprite.isWall();
 			} catch (IndexOutOfBoundsException e) {
 				return false;
