ff92b2c3394d62eb560841cf2e2f4cee45b1a00d
Campbell Yeoman <cam@yeoman.com.au>
2020-11-29 00:00:05 +1100

    finished testing and commenting

--- src/main/java/ghost/Direction.java
+++ src/main/java/ghost/Direction.java
@@ -1,18 +1,26 @@
 package ghost;
 
 public enum Direction {
+
 	up(38),
 	left(37),
 	right(39),
 	down(40);
 	
+	/**
+	 * keyCode associated with direction.
+	 */
 	final int KEY_CODE;
 
 	Direction(int KEY_CODE) {
 		this.KEY_CODE = KEY_CODE;
 	}
 
+	/**
+	 * Get the opposite direction to the current direction.
+	 */
 	Direction getOpposite() {
+
 		if (this == Direction.left) {
 			return right;
 		} if (this == Direction.right) {
@@ -22,14 +30,20 @@ public enum Direction {
 		} else {
 			return up;
 		}
+
 	}
 
+	/**
+	 * Get the opposite direction to the current direction.
+	 */
 	boolean isHorizontal() {
+
 		if (this == right || this == left) {
 			return true;
 		} else {
 			return false;
 		}
-	}
-}
 
+	}
+
+}
