6e6756d9382edbc10f319341a07245f014324509
Campbell Yeoman <cam@yeoman.com.au>
2020-11-29 13:02:49 +1100

    completely finished

--- src/main/java/ghost/Game.java
+++ src/main/java/ghost/Game.java
@@ -9,43 +9,101 @@ import java.util.Queue;
 import java.util.List;
 import java.util.Map;
 
-import processing.core.PApplet;
 import processing.core.PImage;
 import processing.core.PFont;
 
+/**
+ * The game instance controlling all logic.
+ */
 public class Game {
+
 	// Configuration settings
+
+	/**
+	 * ModeController object to control mode.
+	 */
 	final ModeController modeControl;
+
+	/**
+	 * The map as represented by an array of Sprites.\
+	 */
 	final Sprite[][] SPRITE_MAP;
+
+	/**
+	 * The initial lives from a config file.
+	 */
 	final int initialLives;
+
+	/**
+	 * The associated App for rendering.
+	 */
 	final App app;
 
+	/**
+	 * Link sprite objects to PImages for rendering.
+	 */
 	Map<Sprite, PImage> allSprites;
-	List<GameObject> GAME_OBJECTS;
-	List<Point> corners;
-	List<Ghost> GHOSTS;
-	Player PLAYER;
+
+	/**
+	 * List of all game objects in game instance.
+	 */
+	List<GameObject> gameObjects;
+
+	/**
+	 * List of all ghosts in game instance.
+	 */
+	List<Ghost> ghosts;
+
+	/**
+	 * Current player of game instance.
+	 */
+	Player player;
 
 	// Dynamic Attributes
 
+	/**
+	 * If debug mode is active.
+	 */
 	boolean debugMode;
-	Boolean won;
 
+	/**
+	 * If the game has been won.
+	 */
+	boolean won;
+
+	/**
+	 * Counter for end screen, representing how long to wait.
+	 */
 	int waitCounter;
+
+	/**
+	 * Number of frames elapsed in game instance.
+	 */
 	int frames;
+
+	/**
+	 * Number of points in game instance.
+	 */
 	int points;
+
+	/**
+	 * Number of lives in game instance.
+	 */
 	int lives;
 
-	public Game(App app, String configFile)
-	{
+	/**
+	 * Initialise game from config file.
+	 * @param app, rendering app object
+	 * @param configFile, config file to parse
+	 */
+	public Game(App app, String configFile) {
+
+		// setup game instance
 		this.app = app;
+		this.won = false;
 
-		// null means in progress, i.e not won or lost
-		this.won = null;
-
-		GAME_OBJECTS = new ArrayList<>();
-		GHOSTS = new ArrayList<>();
-		corners = new ArrayList<>();
+		gameObjects = new ArrayList<>();
+		ghosts = new ArrayList<>();
 
 		// create config object
 		Configuration config = new Configuration(configFile);
@@ -71,38 +129,40 @@ public class Game {
 
 					if (SPRITE_MAP[j][i] == Sprite.playerRight) {
 						// create player instance
-						PLAYER = new Player(16 * i, 16 * j);
+						player = new Player(16 * i, 16 * j);
 					} else if (SPRITE_MAP[j][i].isGhost()) {
 						// create ghost instances
-						GHOSTS.add(new Ghost(16 * i, 16 * j, SPRITE_MAP[j][i]));
+						ghosts.add(new Ghost(16 * i, 16 * j, SPRITE_MAP[j][i]));
 					} else if (SPRITE_MAP[j][i].isGameObject()) {
 						// create game object instances
-						GAME_OBJECTS.add(new GameObject(16 * i, 16 * j, SPRITE_MAP[j][i]));
+						gameObjects.add(new GameObject(16 * i, 16 * j, SPRITE_MAP[j][i]));
 					}
 
 				}
 			}
 		}
 
-		List<Ghost> chasers = GHOSTS.stream()
-			.filter(g -> g.isAlive() && g.type == Ghost.Type.chaser)
-			.collect(Collectors.toList());
-
-		if (chasers.size() > 0) {
-			Ghost.setChaser(chasers.get(0));
+		// get CHASER
+		for (Ghost ghost : ghosts) {
+			if (ghost.type == Ghost.Type.chaser) {
+				Ghost.setChaser(ghost);
+				break;
+			}
 		}
+
 	}
 
+	/**
+	 * Loads PImages, and links them to their Sprite equivalent, and loads the
+	 * PFont for the end screen.
+	 */
+	public void loadAssets() {
 
-	public void loadAssets()
-	{
 		// load sprites
 		allSprites = new HashMap<>();
 		
 		for (Sprite sprite : Sprite.values()) {
-			if (sprite != null) {
-				allSprites.put(sprite, app.loadImage(sprite.filePath));
-			}
+			allSprites.put(sprite, app.loadImage(sprite.filePath));
 		}
 
 		// load font
@@ -112,15 +172,20 @@ public class Game {
 
 		String filePath = "src/main/resources/PressStart2P-Regular.ttf";
 		app.textFont(app.createFont(filePath, 20, false, letters));
+
 	}
 
 	// Draw methods
 	
-	public void draw(Sprite sprite, int x, int y, Point target)
-	{
-		if (sprite == null) {
-			return;
-		}
+	/**
+	 * Draw a game entity, with infrastructure for debug mode with the target
+	 * point.
+	 * @param sprite, sprite to render
+	 * @param x, the integer x coordinate
+	 * @param y, the integer y coordinate
+	 * @param target, the target for the debug mode line
+	 */
+	public void draw(Sprite sprite, int x, int y, Point target) {
 
 		app.image(allSprites.get(sprite), x, y);
 
@@ -130,40 +195,34 @@ public class Game {
 			app.line(x + 12, y + 12, target.x, target.y);
 			app.endShape();
 		}
-	}
 
-	public void endScreen(boolean won)
-	{
-		app.background(0,0,0);
-
-		if (won) {
-			app.text("You won", 154, 288);
-		} else {
-			app.text("Game Over", 134, 288);
-		}
 	}
 
 	// Tic methods
 
-	public void reset()
-	{
+	/**
+	 * Reset the game entity positions and decrement the game lives, if lives
+	 * greater than 0.
+	 */
+	public void reset() {
+
 		// Reset positions and attributes
-		GHOSTS.stream().forEach(Agent::reset);
-		PLAYER.reset();
+		ghosts.stream().forEach(Agent::reset);
+		player.reset();
 
-		if (lives > 0) {
-			lives--;
-		}
+		// decrement game lives
+		lives = (lives > 0) ? lives - 1 : lives;
 
-		if (won != null) {
-
-		}
 	}
 
-	public void checkWin()
-	{
-		int objectsLeft = (int) GAME_OBJECTS.stream()
-			.filter(obj -> !obj.isEaten())
+	/**
+	 * Check if the game has been won or lost, then it is over transition to
+	 * end screen for 10 seconds.
+	 */
+	public void checkWin() {
+
+		int objectsLeft = (int) gameObjects.stream()
+			.filter(obj -> !obj.isEaten() && obj.type != GameObject.Type.soda)
 			.count();
 
 		if (objectsLeft == 0 || lives == 0) {
@@ -172,9 +231,9 @@ public class Game {
 			won = (lives == 0) ? false : true;
 
 			// reset all game entities
-			GHOSTS.stream().forEach(Agent::reset);
-			PLAYER.reset();
-			GAME_OBJECTS.stream().forEach(GameObject::reset);
+			ghosts.stream().forEach(Agent::reset);
+			player.reset();
+			gameObjects.stream().forEach(GameObject::reset);
 
 			// reset game attributes
 			frames = 0;
@@ -184,79 +243,94 @@ public class Game {
 			// Que 10 seconds of waiting with end screen
 			waitCounter = 60 * 10;
 		}
+
 	}
 
-	public void drawGame()
-	{
+	/**
+	 * Draw the game map, background, scoreboard, gameObjects, ghosts and player.
+	 */
+	public void drawGame() {
+
 		// Draw map
 		app.background(0,0,0);
 
 		for (int j=0; j < 36; j++) {
 			for (int i=0; i < 28; i++) {
 				if (SPRITE_MAP[j][i] != null && SPRITE_MAP[j][i].isWall()) {
-					if (allSprites.get(SPRITE_MAP[j][i]) != null) {
-						app.image(allSprites.get(SPRITE_MAP[j][i]), 16 * i, 16 * j);
-					}
+					draw(SPRITE_MAP[j][i], 16 * i, 16 * j, null);
 				}
 			}
 		}
 
 		// draw scoreboard
 		for (int i=0; i < lives; i++) {
-			app.image(allSprites.get(Sprite.playerRight), 8 + 27 * i, 543);
+			draw(Sprite.playerRight, 8 + 27 * i, 543, null);
 		}
 
 		// Draw game entities
 
-		if (PLAYER != null) {
-			PLAYER.draw(this);
-		}
+		player.draw(this);
 
-		for (GameObject obj : GAME_OBJECTS) {
-			if (!obj.isEaten()) {
-				obj.draw(this);
-			}
-		}
+		gameObjects.stream()
+			.filter(obj -> !obj.isEaten())
+			.forEach(obj -> obj.draw(this));
 
-		for (Ghost ghost : GHOSTS) {
-			if (ghost.isAlive()) {
-				ghost.draw(this);
-			}
-		}
+		ghosts.stream()
+			.filter(ghost -> ghost.isAlive())
+			.forEach(ghost -> ghost.draw(this));
+
 	}
 
-	public void ticGame()
-	{
-		Ghost.MODE = modeControl.update();
+	/**
+	 * Update game entities, debug mode and increment the number of frames by 1.
+	 */
+	public void ticGame() {
 
-		if (app.keyCode == 32) {
-			debugMode = (debugMode) ? false : true;
-		}
+		Ghost.setMode(modeControl.update());
 
-		PLAYER.TIC(this);
+		debugMode = (app.keyCode == 32) ? !debugMode : debugMode;
+
+		player.TIC(this);
 		GameObject.TIC(this);
 		Ghost.TIC(this);
 
 		frames++;
+
 	}
 
-	public void run()
-	{
-		if (waitCounter < 1) {
-			if (won != null) {
-				won = null;
-			}
+	/**
+	 * Core loop to update and draw the game.
+	 */
+	public void run() {
 
+		if (waitCounter == 0) {
 			drawGame();
 			ticGame();
 			checkWin();
 		} else {
-			// only need to draw once
-			if (waitCounter == 60 * 10) {
-				endScreen(won);
+
+			// draw win/loss screen
+
+			app.background(0,0,0);
+
+			if (won) {
+				app.text("You won", 154, 288);
+			} else {
+				app.text("Game Over", 134, 288);
 			}
 
-			waitCounter--;
+			// increment counter and if wait is over reset game
+
+			if (waitCounter > 1) {
+				waitCounter--;
+			} else {
+				gameObjects.stream().forEach(GameObject::reset);
+				won = false;
+				reset();
+			}
+
 		}
+
 	}
+
 }
